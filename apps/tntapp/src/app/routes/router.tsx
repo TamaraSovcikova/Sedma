b1cd2ef332712ddc29e278b083475ffedd0722d0
snaccident <tamara@sovcik.net>
2023-08-05 17:23:54 +0100

    websocket implemented with login page

--- apps/tntapp/src/app/routes/router.tsx
+++ apps/tntapp/src/app/routes/router.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { InitialPage } from '../pages/initial-page';
-import { TablePage } from '../pages/table-page';
+import { InitialPage } from '../pages/initial-page/index';
+import { TablePage } from '../pages/table-page/index';
+import { LoginPage } from '../pages/login-page/login-page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <InitialPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
   },
   {
     path: '/table/:id',
