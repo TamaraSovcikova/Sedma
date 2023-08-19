0ac19d099f5758a457355c0bb16abe182a5f92fc
Jozef Sovcik <jozef.sovcik@gmail.com>
2023-08-19 21:03:49 +0200

    configuing api url

--- apps/tntapp/src/app/global.ts
+++ apps/tntapp/src/app/global.ts
@@ -1,5 +1,19 @@
+declare global {
+  interface Window {
+    NX_SERVER_API_URL?: string | null;
+  }
+}
+
+/**
+ * Server API URL.\
+ * Specific url is set during installation by modifying file assets/variables.js \
+ * Default value is set by the build process using the environment variable NX_API_URL.\
+ * If the environment variable is not set, then the default value is 'http://localhost:3000'.
+ */
+const rootApiUrl = window.NX_SERVER_API_URL ?? process.env['NX_SERVER_API_URL'] ?? 'http://localhost:3000'
+
 export const getServerUrl = () => ({
-  serverUrl: 'http://localhost:3000/api',
-  tableUrl: (id: string) => `http://localhost:3000/table/${id}`,
-  lobbyUrl: (id: string) => `http://localhost:3000/table/lobby/${id}`,
+  serverUrl: `${rootApiUrl}/api`,
+  tableUrl: (id: string) => `${rootApiUrl}/table/${id}`,
+  lobbyUrl: (id: string) => `${rootApiUrl}/table/lobby/${id}`,
 });
