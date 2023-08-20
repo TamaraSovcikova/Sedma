19191a3bb216c8b9d0f1bd69287a8f5525a5d91c
Jozef Sovcik <jozef.sovcik@gmail.com>
2023-08-20 08:40:37 +0200

    removing host binding

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,13 +6,11 @@ import { createRoutes } from './routes/routes';
 import { createDummyData } from './lib/dummy-data';
 import { getConfig } from './global';
 
-const host = 'localhost';
 const port = 3000;
 const clientUrl = getConfig().clientUrl;
 
 const corsOptions = {
-  origin: ['*',clientUrl], // enable access from client app only
-  optionsSuccessStatus: 200,
+  origin: [clientUrl], // enable access from client app only
   preflightContinue: false,
   credentials: false,
 };
@@ -29,7 +27,7 @@ app.use(bodyParser.json());
 createRoutes(app);
 createDummyData();
 
-app.listen(port, host, () => {
-  console.log(`api listening at http://${host}:${port}`);
+app.listen(port, () => {
+  console.log(`api listening at port=${port}`);
   console.log(`client-url=${clientUrl}`);
 });
