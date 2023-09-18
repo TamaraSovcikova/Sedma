64880530f08345e85c97ee47f9cff5ecb037739e
snaccident <tamara@sovcik.net>
2023-09-18 20:55:03 +0100

    styled lobby page, added return button, tableid

--- apps/tntapp/src/app/pages/table.lobby-page/table.lobby-page.tsx
+++ apps/tntapp/src/app/pages/table.lobby-page/table.lobby-page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../../styles/table.lobby-page.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import { fetchData, postData } from '../../lib/api';
@@ -126,45 +126,120 @@ export function LobbyPage() {
     console.log(`${username} just chose a seat and is ready to play`);
     setToken(newtoken);
   };
+  const handleReturn = () => {
+    navigate('/');
+  };
+
+  const handleCopy = () => {
+    const inputElement = document.getElementById(
+      'table-id-input'
+    ) as HTMLInputElement;
+    const textToCopy = inputElement?.value || '';
+
+    if (textToCopy) {
+      const textArea = document.createElement('textarea');
+      textArea.value = textToCopy;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+
+      alert('Text copied to clipboard!');
+    } else {
+      alert('Please enter a Table ID to copy.');
+    }
+  };
 
   return (
-    <div className="lobby-container">
-      <div className="lobby-info">
-        <h2 className="lobby-header">Welcome to a game of Sedma</h2>
-        <p>
-          Please enter your name and choose a seat you wish to be seated at.
-          (PS. Players opposite each other will be together in a team)
-        </p>
+    <div className="container">
+      <button className="btn btn-secondary returnButton" onClick={handleReturn}>
+        <i
+          className="fas fa-arrow-left"
+          style={{ fontSize: '20px', color: 'black' }}
+        ></i>
+      </button>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-8">
+          <h2 className="lobby-header text-center">JOIN A GAME OF SEDMA</h2>
+        </div>
+        <div className="row justify-content-center mt-4">
+          <div className="col-md-8">
+            <p className="tableID-text">
+              Your Unique Table ID, send it to your friends to let them join!
+            </p>
+            <div className="input-group mt-5 mb-3">
+              <input
+                id="table-id-input"
+                type="text"
+                className="form-control"
+                name="Table ID"
+                placeholder="Table ID"
+                value={id}
+                readOnly
+              />
+              <button className="btn btn-secondary m-0" onClick={handleCopy}>
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="mt-5 separator"></div>
       </div>
-      <div className="lobby-setting-params">
-        <label id="lobby-label">
-          Username:
-          <input
-            id="username-input"
-            type="text"
-            name="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </label>
-        <div className="teams-container">
-          <Team
-            teamName="Team A"
-            seats={[seats[0], seats[2]]}
-            selectedSeatId={selectedSeatId}
-            onSeatClick={handleSeatClick}
-          />
-          <Team
-            teamName="Team B"
-            seats={[seats[1], seats[3]]}
-            selectedSeatId={selectedSeatId}
-            onSeatClick={handleSeatClick}
-          />
+      <div className="row justify-content-center mt-4">
+        <div className="col-md-8">
+          <div className="form-group">
+            <label style={{ fontSize: '20px' }} htmlFor="username-input">
+              ENTER USERNAME:
+            </label>
+            <input
+              id="username-input"
+              type="text"
+              className="form-control"
+              name="Username"
+              placeholder="Username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
         </div>
       </div>
-      <button className="lobby-button" onClick={handleContinue}>
-        Continue
-      </button>
+      <p className="teamInfo">
+        After entering your username, please select the seat you want to be
+        seated at whilst simultaneously choosing your teamate. Pick any of the
+        'Available' slots.
+      </p>
+      <div className="row justify-content-center mt-4">
+        <div className="col-md-8">
+          <div className="row teams-container">
+            <div className="col-md-6 team">
+              <Team
+                teamName="Team A"
+                seats={[seats[0], seats[2]]}
+                selectedSeatId={selectedSeatId}
+                onSeatClick={handleSeatClick}
+              />
+            </div>
+            <div className="col-md-6 team">
+              <Team
+                teamName="Team B"
+                seats={[seats[1], seats[3]]}
+                selectedSeatId={selectedSeatId}
+                onSeatClick={handleSeatClick}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-md-12 mb-5">
+          <button
+            className="btn btn-primary lobby-button"
+            onClick={handleContinue}
+          >
+            Continue
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
