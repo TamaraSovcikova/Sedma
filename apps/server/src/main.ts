44ddfdc75e0d08b4757b0d66e7cc5ed2ead6506b
snaccident <tamara@sovcik.net>
2023-10-14 20:27:41 +0100

    server deployment adaptation

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,11 +4,11 @@ import bodyParser from 'body-parser';
 import { createWebSocketServer } from './lib/wsServer';
 import { createRoutes } from './routes/routes';
 import debugLog from 'debug';
+import { getAppUrl } from './global';
 
 const debug = debugLog('main');
 debugLog.enable('* -routes wsServer table -table.sendUpdates -computerPlayer1');
 
-const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
@@ -17,12 +17,12 @@ createWebSocketServer();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: 'http://localhost:4200',
+    origin: [getAppUrl().clientUrl],
   })
 );
 
 createRoutes(app);
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+app.listen(port, () => {
+  console.log(`[ ready ] http://${port}`);
 });
