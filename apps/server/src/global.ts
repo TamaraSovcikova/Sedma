a01997924b4d4299437f204c3d132c5488f3ee44
snaccident <tamara@sovcik.net>
2023-10-15 13:32:52 +0100

    fixing parametrization

--- apps/server/src/global.ts
+++ apps/server/src/global.ts
@@ -1,5 +1,5 @@
 export const getConfig = () => ({
   clientUrl: process.env.CLIENT_URL ?? 'http://localhost:4200',
-  wsPort: Number(process.env.WS_PORT) ?? 4000,
-  port: Number(process.env.PORT) ?? 3000,
+  wsPort: process.env.WS_PORT ? Number(process.env.WS_PORT) : 4500,
+  port: process.env.PORT ? Number(process.env.PORT) : 3000,
 });
