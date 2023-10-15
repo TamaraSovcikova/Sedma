60a93844d286ddcacf9a220eed486f130dc17ff4
snaccident <tamara@sovcik.net>
2023-10-15 13:22:23 +0100

    websocket port, port made configurable

--- apps/server/src/global.ts
+++ apps/server/src/global.ts
@@ -1,3 +1,5 @@
-export const getAppUrl = () => ({
+export const getConfig = () => ({
   clientUrl: process.env.CLIENT_URL ?? 'http://localhost:4200',
+  wsPort: Number(process.env.WS_PORT) ?? 4000,
+  port: Number(process.env.PORT) ?? 3000,
 });
