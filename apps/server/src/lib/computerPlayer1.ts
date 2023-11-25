305a71e4af8ff2a8a6f054101cf90d27f9a2cd67
snaccident <tamara@sovcik.net>
2023-11-25 11:49:00 +0000

    adding come comments

--- apps/server/src/lib/computerPlayer1.ts
+++ apps/server/src/lib/computerPlayer1.ts
@@ -1,23 +1,31 @@
 import { AutoPlay } from './player';
 import { Table } from './table';
 import debugLog from 'debug';
+
 const debug = debugLog('computerPlayer1');
 
 export const computerLevel1: AutoPlay = (table: Table, playerIdx: number) => {
   const player = table.players[playerIdx];
+
+  // Check if it's the bots turn to play
   if (playerIdx !== table.currentPlayer) return;
+
+  // Check if bot has no cards left
   if (player.onHand.length === 0) {
     table.endRound();
-    console.log('computer player', player.name, 'has no cards');
+    console.log('Computer player', player.name, 'has no cards');
     return;
   }
 
+  // Check if bot has a card to beat the current leading player
   const findCard = player.onHand.find(
     (c) => c.face === table.cardToBeat?.face || c.face === 'seven'
   );
 
   debug(
-    `tableId = ${table.id} playerIdx = ${playerIdx} onHand = ${player.onHand}`
+    `Playing turn: tableId=${table.id}, playerIdx=${playerIdx}, onHand=${player.onHand}`
   );
+
+  // Play the selected card or the first card in hand if no match is found
   table.playCard(player.id, findCard || player.onHand[0]);
 };
