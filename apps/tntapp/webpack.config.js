dc8b6ed63f8e2b146d4f5990d43a2c99858f143d
snaccident <tamara@sovcik.net>
2024-01-25 21:06:31 +0000

    assets

--- apps/tntapp/webpack.config.js
+++ apps/tntapp/webpack.config.js
@@ -4,6 +4,7 @@ const { withReact } = require('@nx/react');
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
+  config.output.publicPath = '/assets/'
   // e.g. `config.plugins.push(new MyPlugin())`
   return config;
 });
